8cd3db2036e1e718d3e118c7826130a570bac676
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2021-12-24 13:14:51 +0100

    Green function implementation and test.

--- patyczki/green_functions.py
+++ patyczki/green_functions.py
@@ -1,2 +1,35 @@
+import numpy as np
 
 
+def green(x, y, x0, y0, w=1.0):
+    """
+    Greens function to Laplaces equation in semi-infinite strip, with reflective
+    walls and absorbing floor.
+
+    Parameters
+    ----------
+    x : float
+        x coordinate of sample point
+    y : float
+        y coordinate of sample point
+    x0 : float
+        x coordinate of point source
+    y0 : float
+        y coordinate of point source
+    w : float, default 1.0
+        *half* width of the strip
+
+    Returns
+    -------
+    float
+        value of Greens function at loction (x,y) with source located at (x0,y0).
+
+    """
+
+    return (
+        (np.cos(np.pi * (x - x0) / w) - np.cosh(np.pi * (y - y0) / w))
+        * (np.cos(np.pi * (x + x0) / w) - np.cosh(np.pi * (y - y0) / w))
+    ) / (
+        (np.cos(np.pi * (x - x0) / w) - np.cosh(np.pi * (y + y0) / w))
+        * (np.cos(np.pi * (x + x0) / w) - np.cosh(np.pi * (y + y0) / w))
+    )
--- tests/test_green_function.py
+++ tests/test_green_function.py
@@ -1,9 +1,18 @@
 import patyczki
-import numpy
+import numpy as np
 
 def test_green_function_values():
 
-  a = 2.0
+  xpts = np.arange(0.1,0.71,0.1)
 
-  assert numpy.isclose(a, 2.0, atol=1e-6)
+  y = 1.4
+  y0 = 2.1
+  x0 = 1.7
+  w = 3.0
+
+  result = np.array([[x, patyczki.green_functions.green(x,y,x0,y0,w)] for x in xpts])
+
+  target = np.array([[0.1,0.00564661],[0.2,0.0055438],[0.3,0.00537652],[0.4,0.00515071],[0.5,0.00487436],[0.6,0.00455722],[0.7,0.00421043]])
+
+  assert np.isclose(result, target, atol=1e-6).all()
   
